2a79f405f97150f94af3b4b6e94b2965d783a4c9
Michael Kozub <michaelkozub@gmail.com>
2018-11-07 18:01:17 -0700

    initial commit

---
+++ autoLayout_ios_6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autoLayout_ios_6
+//
+//  Created by Michael Kozub on 11/7/18.
+//  Copyright © 2018 Michael Kozub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
